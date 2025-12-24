7ff4739f3d8a6e1028df5674ae07e7e6dae3efad
Thomas Delplace <thomasrenedelplace@gmail.com>
2025-12-24 01:04:52 +0100

    replaced hardcoded for env variables

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,7 @@ export default defineConfig({
     react()
   ],
   server:{
-    // host:'192.168.1.33', // réseau maison
-    // host:'192.168.154.103', // réseau Zoa
-    host:'localhost', // général
-    port:'3001'
+    host:import.meta.env.VITE_APP_URL,
+    port:import.meta.env.VITE_APP_PORT
   }
 })
