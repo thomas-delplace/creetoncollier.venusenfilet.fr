03e8148895abe8d12d144b7d95630cabc4f64c5e
Thomas Delplace <thomasrenedelplace@gmail.com>
2025-12-25 20:22:07 +0100

    repaired weird bug 'undefined VITE_APP_URL' with usage of loadEnv method

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react()
-  ],
-  server:{
-    host:import.meta.env.VITE_APP_URL,
-    port:import.meta.env.VITE_APP_PORT
-  }
-})
+export default ({ mode }) => {
+  // eslint-disable-next-line no-undef
+  const env = loadEnv(mode, process.cwd(), 'VITE_')
+  return defineConfig({
+    plugins: [
+      react()
+    ],
+    define: {
+      'process.env': env
+    },
+    server:{
+      host:env.VITE_APP_URL,
+      port:env.VITE_APP_PORT
+    }
+  })
+}
